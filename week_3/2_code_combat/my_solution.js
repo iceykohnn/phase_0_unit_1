8f7a111596b4742f523d05956bd465e176f45cf8
Isaac <iceykohnn@gmail.com>
2014-05-11 22:27:08 -0500

    updated my code combat reflection

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,10 +1,11 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself.
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
- 
+ // I completed the levels, before I looked at these directions. I didn't pseudocode and when I tried 
+ // to get back to redo the levels, they made me continue on past the beginner campaign. 
  
  
  
@@ -14,3 +15,13 @@
  
 // Reflection:
 // Write your reflection here.
+
+// The code combat idea, website and game are all brilliant. I had a lot of fun playing and figuring out how 
+// much work actually goes into all of my video games.  Also, it felt good to get away, even if not really, 
+// from the normal thougt pattern of coding.  I enjoyed seeing what I was doing, and at the same time playing 
+// as the time wizard on my little floating cloud. Whe I play games, I usually approach them so instinctively, 
+// never really putting much thought into how much code went into defining the movements, and the strategy of 
+// how many different ways the game could be played. The mission where I had to get the mushroom power up before
+// I could kill the enemy, was fun, but the one where I had to taunt the guard, and get him to line up so my 
+// archer could shoot him was the tough one. I was stuck on that one trying to get by the last two guards....
+// but it was a grand way to look at Javascript. I had so much fun. 
