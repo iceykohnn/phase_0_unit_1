83953e2fd09c5974536c082acda4f0c5445ef4e9
Iceykohnn <iceykohnn@gmail.com>
2014-05-11 23:49:40 -0500

    updated js_variables mysolution

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -4,35 +4,49 @@
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
+// assign the value of secretNumber a number value
+// change the value of secretNumber to 7
+// make the value of password equal a string
+// make the string value of password change to 'just open the door'
+// make the value of allowedIn a boolean (meaning true or false)
+// change allowedIn value to false
+//assign the value of members to an array
+// give the first value in the array members the value of john
+// give the fourth value in the array members the value of mary
 
 
 // __________________________________________
 // Write your code below.
 
-
-
-
+var secretNumber = 2
+secretNumber = 7
+password = "w00t"
+password = 'just open the door'
+var allowedIn = true
+allowedIn = false
+var members = []
+members = ['john',]
+members = ['John',]
+members = ['Mary']
 
 
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
 
-
-
+// i had to change the value of ['john'] to ['John']
+members =['John']
+// i didn't focus and I accidently changed the value of John into the value of Mary
+members = ['John',,,'Mary']
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
-// 
-// 
-// 
-// 
+// I was confused about this exercise at first, but that was because I was over thinking it. When I went back 
+// and followed the instructions, I was ok. The hardest thing to do turned into not just fixing my mistakes. I 
+// was tempted to edit my work and put in the correct answer with out having anything to put into the refactored
+// area.  But I think that I am getting the hang of this pseudocode thing. 
 // 
 
 
